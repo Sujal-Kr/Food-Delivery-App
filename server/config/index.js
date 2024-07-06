aaa39a0c78d5b89acbbac2e7dd8a87647ea75c55
Sujal-Kr <sunnykumarcms45@gmail.com>
2024-07-06 12:12:25 +0530

    Remove files from tracking and update .gitignore

--- server/config/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const URL=process.env.URL
-
-mongoose.connect(URL).then(db=>{
-    console.log("Connected to Database ");
-}).catch(error=>{
-    console.log("error: " + error);
-})
